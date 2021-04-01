7cfe5bbc3565424c64286f8202298a4737ee2db0
BlackWidowRoms <71955401+blackwidowroms@users.noreply.github.com>
2021-03-31 21:28:33 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,3 +80,23 @@ TW_USE_NEW_MINADBD := true
 TW_INCLUDE_FBE := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#Ignore Missing Dependencies
+ALLOW_MISSING_DEPENDENCIES=true
+
+
+#SHRP_Variables
+SHRP_PATH := device/samsung/j4lte
+SHRP_MAINTAINER := BlackRomsDev
+SHRP_DEVICE_CODE := j4lte
+SHRP_EDL_MODE := 1
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usbotg
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_REC_TYPE := Treble
+SHRP_DEVICE_TYPE := A/B
+SHRP_FLASH := 1
+#SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
+#SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
+#SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+#SHRP_FLASH_MAX_BRIGHTNESS := 200
