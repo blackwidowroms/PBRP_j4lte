a6a4f9268fce7c8fcb0934bc22a2723dd35ae673
BlackWidowRoms <71955401+blackwidowroms@users.noreply.github.com>
2021-08-05 10:48:07 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,11 +92,13 @@ SHRP_EDL_MODE := 1
 SHRP_EXTERNAL := /external_sd
 SHRP_INTERNAL := /sdcard
 SHRP_OTG := /usbotg
-SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_REC := /dev/block/platform/13540000.dwmmc0# Shrp official
+SHRP_OFFICIAL := true# Shrp official
+SHRP_OFFICIAL := true/by-name/recovery
 SHRP_REC_TYPE := Treble
 SHRP_DEVICE_TYPE := A/B
 SHRP_FLASH := 1
-#SHRP_FONP_1 := /sys/class/leds/led:torch_0/brightness
-#SHRP_FONP_2 := /sys/class/leds/led:torch_1/brightness
-#SHRP_FONP_3 := /sys/class/leds/led:switch/brightness
+# Shrp official
+SHRP_OFFICIAL := true
 #SHRP_FLASH_MAX_BRIGHTNESS := 200
+SHRP_OFFICIAL := true
